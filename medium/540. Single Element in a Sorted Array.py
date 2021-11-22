f695177cc59350daaebdb106249240a724adb411
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2021-11-21 18:13:59 -0600

    :(: corrected
    Time complexity: O(log n)
    Space complexity: O(1)
    Runtime: 76 ms
    Memory Usage: 16.4 MB
    
    Time developed: +1 hr

--- medium/540. Single Element in a Sorted Array.py
+++ medium/540. Single Element in a Sorted Array.py
@@ -1,10 +1,26 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        i = 0        
-        while i < len(nums):
-            if i+1 < len(nums) and nums[i] != nums[i+1]:
-                return nums[i]
-            elif i+1 >= len(nums):
-                return nums[i]
-            i += 2
-        return nums[0]
+        bottom = 0
+        upper = len(nums) - 1
+        
+        # return in boundaries if it is unique number
+        if bottom == upper:
+            return nums[0]
+        if nums[bottom] != nums[bottom+1]:
+            return nums[bottom]
+        if nums[upper] != nums[upper-1]:
+            return nums[upper]
+            
+        while bottom <= upper:
+            middle = bottom + int((upper - bottom) / 2)
+            
+            if nums[middle] != nums[middle-1] and nums[middle] != nums[middle+1]:
+                return nums[middle]
+            
+            # decide which part to analyze
+            if (middle%2 == 0 and nums[middle] == nums[middle+1]) or (middle%2 == 1 and nums[middle] == nums[middle-1]):
+                bottom = middle + 1
+            else:
+                upper = middle - 1
+        
+        return -1
